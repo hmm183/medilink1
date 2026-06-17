a477b0019f483ab088ea46c107a418a16bf8b899
hmm183 <vrishankraina@gmail.com>
2026-06-17 13:24:35 +0530

    Update API base URL to production Render server

--- lib/core/config/AppConfig.dart
+++ lib/core/config/AppConfig.dart
@@ -1,6 +1,3 @@
 class AppConfig {
-  // Points to local backend server. For physical USB devices, run `adb reverse tcp:5000 tcp:5000`
-  static const String baseUrl = "http://127.0.0.1:5000/api/v1";
-  // Remote production server:
-  // static const String baseUrl = "https://newmediback.onrender.com/api/v1";
+  static const String baseUrl = "https://medilink1-kgb3.onrender.com/api/v1";
 }
